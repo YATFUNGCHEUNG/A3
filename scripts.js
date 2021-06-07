24626d75b2bae476a15a69ec259de2f41462765f
YATFUNGCHEUNG <57380926+YATFUNGCHEUNG@users.noreply.github.com>
2021-06-07 23:26:39 +0800

    style

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const lineColors = {
+    green: "#44bb66",
+    red: "#bb4466",
+    blue: "#4466bb"
+}
+
+const config = {
+    displayModeBar: false,
+    responsive: true
+}
+
+const plotDiv = document.getElgementById("vis1");
+const plotDiv = document.getElgementById("vis2");
+
+Plotly.d3.csv("csv", function(rows){
+
+});
