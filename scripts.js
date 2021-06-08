ad7aa09d823ca8bdbf3b6cfce6d5b7174d626c83
YATFUNGCHEUNG <57380926+YATFUNGCHEUNG@users.noreply.github.com>
2021-06-09 00:54:20 +0800

    yo

--- scripts.js
+++ scripts.js
@@ -12,31 +12,20 @@ const config = {
 const plot1Div = document.getElementById("vis1");
 const plot2Div = document.getElementById("vis2");
 
-Plotly.d3.csv("choice of word - Sheet1.csv", function(rows){
+Plotly.d3.csv("sentiment_value.csv", function(rows){
 
     var trace0= {
         type: "scatter",
         mode: "lines",
         name: "APPL High",
         x: unpack(rows, "Date"),
-        y: unpack(rows, "AAPL.High"),
-        line: {
-            color: lineColors.green
-        }
-    }
-
-    var trace1= {
-        type: "scatter",
-        mode: "lines",
-        name: "APPL High",
-        x: unpack(rows, "Date"),
-        y: unpack(rows, "AAPL.Low"),
+        y: unpack(rows, "Sentiment value"),
         line: {
             color: lineColors.red
         }
     }
 
-    var data1 = [trace0, trace1];
+    var data1 = [trace0];
 
     var layout1 = {
         title: "meow",
