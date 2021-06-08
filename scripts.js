bf7a35f4b95640209c47f8584835bfb686a4bf7d
YATFUNGCHEUNG <57380926+YATFUNGCHEUNG@users.noreply.github.com>
2021-06-09 00:44:54 +0800

    yo

--- scripts.js
+++ scripts.js
@@ -9,9 +9,63 @@ const config = {
     responsive: true
 }
 
-const plotDiv = document.getElgementById("vis1");
-const plotDiv = document.getElgementById("vis2");
+const plot1Div = document.getElementById("vis1");
+const plot2Div = document.getElementById("vis2");
 
-Plotly.d3.csv("csv", function(rows){
+Plotly.d3.csv("choice of word - Sheet1.csv", function(rows){
 
+    var trace0= {
+        type: "scatter",
+        mode: "lines",
+        name: "APPL High",
+        x: unpack(rows, "Date"),
+        y: unpack(rows, "AAPL.High"),
+        line: {
+            color: lineColors.green
+        }
+    }
+
+    var trace1= {
+        type: "scatter",
+        mode: "lines",
+        name: "APPL High",
+        x: unpack(rows, "Date"),
+        y: unpack(rows, "AAPL.Low"),
+        line: {
+            color: lineColors.red
+        }
+    }
+
+    var data1 = [trace0, trace1];
+
+    var layout1 = {
+        title: "meow",
+        xaxis: {
+            autorange: true,
+            rangeselector: {
+                buttons: [{
+                        count: 1,
+                        label: '1m',
+                        step: 'month',
+                        stepmode: 'backward'
+                    },
+                    {
+                        count: 6,
+                        label: '6m',
+                        step: 'month',
+                        stepmode: 'backward'
+                    }, {
+                        step: 'all'
+                    }    
+                ]
+            },
+            type: 'date'
+        }
+    }
+
+    Plotly.newPlot(plot1Div, data1, layout1, config);
 });
+
+function unpack(rows, key) {
+    return rows.map(function(row) {return row[key];});
+}
